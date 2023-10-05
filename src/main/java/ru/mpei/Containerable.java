2bb480c36abe63a3d27804fe9e5184288c7769d4
lt.Ky3mu4 <rogozinnikovei@yandex.ru>
2023-10-05 22:49:25 +0300

    added tests

--- src/main/java/ru/mpei/Containerable.java
+++ src/main/java/ru/mpei/Containerable.java
@@ -1,5 +1,8 @@
 package ru.mpei;
 
+/**
+ * Interface which allows getting access to inner data for TripletDeque
+ */
 public interface Containerable{
 
     Object[] getContainerByIndex(int cIndex);
